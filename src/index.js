98d88a206b1a8c31a961e26cb28ae9d2325022a2
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-22 18:25:14 +0300

    Add icons and update to do list div styles

--- src/index.js
+++ src/index.js
@@ -1,21 +1,74 @@
 import './style.css';
 import Tick from './assets/tick-box.png';
+import ViewMore from './assets/view-more.png';
+import Refresh from './assets/refresh.png';
+
+const task1 = {
+  index: 0,
+  completed: true,
+  description: 'Wash the car',
+};
+const task2 = {
+  index: 1,
+  completed: true,
+  description: 'Wash the car',
+};
+const task3 = {
+  index: 2,
+  completed: true,
+  description: 'Wash the car',
+};
+const task4 = {
+  index: 3,
+  completed: true,
+  description: 'Wash the car',
+};
+const task5 = {
+  index: 4,
+  completed: true,
+  description: 'Wash the car',
+};
+
+const toDoList = [task1, task2, task3, task4, task5];
 
 function component() {
   const container = document.createElement('div');
+  container.className = 'main-container';
   const logoElement = document.createElement('div');
+  logoElement.className = 'logo-img';
   const listDiv = document.createElement('div');
+  listDiv.className = 'list-div';
+  const listDivTitle = document.createElement('div');
+  listDivTitle.className = 'title';
   const listTitle = document.createElement('h4');
   listTitle.textContent = 'My To Do List';
+  listDivTitle.appendChild(listTitle);
+  listDiv.appendChild(listDivTitle);
+
+  toDoList.forEach((list) => {
+    const listItem = document.createElement('div');
+    listItem.className = 'to-do-item';
+    listItem.innerHTML = `
+          <input id="done" type="checkbox" value="done">
+          <input type="text" value="${list.description}" class="new-list-item">
+          <img class="view-more" src="${ViewMore}" alt="view-more">
+    `;
+    listDiv.appendChild(listItem);
+  });
 
   // Add list icon
   const tickIcon = new Image();
   tickIcon.src = Tick;
   logoElement.appendChild(tickIcon);
 
+  const refereshIconDiv = document.createElement('div');
+
+  const refreshIcon = new Image();
+  refreshIcon.src = Refresh;
+  refereshIconDiv.appendChild(refreshIcon);
+  listDivTitle.appendChild(refereshIconDiv);
+
   // Create card to hold to do list
-  listDiv.className = 'list-div';
-  listDiv.appendChild(listTitle);
 
   // Append elements to container
   container.appendChild(logoElement);
