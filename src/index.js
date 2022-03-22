7fe8dbf8c3cf01ec38cc6aec2f3f349a80e693b2
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-22 13:03:54 +0300

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import './style.css';
+// import _ from 'lodash';
+
+// function component() {
+//   const element = document.createElement('div');
+
+//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+//   return element;
+// }
+
+// document.body.appendChild(component())
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+};
