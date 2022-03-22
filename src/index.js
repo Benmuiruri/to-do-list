d1846b3275e3aae2cf3d39a96bead3e22b60abb3
Benmuiruri <ben.muiruri13@gmail.com>
2022-03-22 16:30:32 +0300

    Add to do list container

--- src/index.js
+++ src/index.js
@@ -4,12 +4,22 @@ import Tick from './assets/tick-box.png';
 function component() {
   const container = document.createElement('div');
   const logoElement = document.createElement('div');
+  const listDiv = document.createElement('div');
+  const listTitle = document.createElement('h4');
+  listTitle.textContent = 'My To Do List';
 
+  // Add list icon
   const tickIcon = new Image();
   tickIcon.src = Tick;
-
   logoElement.appendChild(tickIcon);
+
+  // Create card to hold to do list
+  listDiv.className = 'list-div';
+  listDiv.appendChild(listTitle);
+
+  // Append elements to container
   container.appendChild(logoElement);
+  container.appendChild(listDiv);
 
   return container;
 }
